d2c3369a3de50db9eff64f777b892e5331791c45
Rockhentai <626707841@qq.com>
2016-12-04 22:21:48 +0800

    Landing.vue

---
+++ src/components/LandingView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="landing">
+    <div id="landing-background" :style="backgroundStyle"></div>
+    <div id="landing-content">
+      <h1 id="landing-title" v-if="config.title">{{config.title}}</h1>
+      <p id="landing-desc">{{{config.description}}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/router-components/Landing.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="landing">
+    <landing-view :config="config"></landing-view>
+    <content :categories="categories"></content>
+  </div>
+</template>
+
+<script>
+import LandingView from '../components/LandingView.vue'
+import Content from '../components/Content.vue'
+
+export default {
+  data() {
+    return {
+      config:{},
+      categories:[]
+    }
+  },
+
+  ready() {
+    Promise.all([
+      Config.load(),
+      Category.load()
+    ])
+      .then(([config,categories]) => {
+        this.config = config
+        this.categories = categories
+      })
+  },
+  
+  components:{
+    LandingView,
+    Content
+  }
+}
+</script>
+
+<style lang="css">
+</style>
