26d28a086e8f42f49a9d99155fd8103db9180231
Rockhentai <626707841@qq.com>
2016-12-25 22:31:09 +0800

    dashboard

---
+++ src/components/admin/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="sidebar" v-el:sidebar>
+    <h2>mylu Admin</h2>
+    <ul class="nav nav-pills nav-stacked">
+      <li role="presentation" :class="{active:active=='dashboard'}"><a href="#!/dashbord">{{'dashboard' | i18n}}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
